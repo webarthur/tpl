8d5c33cf297f24cc6518f51be849daaa9f23fcca
Arthur Araújo <webarthur@gmail.com>
2015-12-04 13:18:40 -0200

    tpl.js

---
+++ tpl.js
@@ -0,0 +1,24 @@
+$tpl = function(template, scope){
+  
+  $tpl.tags || ($tpl.tags = ['{', '}']);
+ 	
+  this!=window
+    && (scope=template, template=this);
+  
+  typeof scope!='Undefined'
+    || (scope=window);
+  
+  return template.replace(new RegExp($tpl.tags[0]+'([^\\'+$tpl.tags[1][0]+']*)'+$tpl.tags[1], 'g'), 
+    function(match, attr){
+      try{
+        with(scope)
+        	return eval(attr) || '';
+      } catch (e) {
+        return '';
+      }
+
+    });
+}
+
+if(!String.prototype.compile)
+  String.prototype.tpl = $tpl;
